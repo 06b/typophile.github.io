a9c356ec2d94e5f486b568c131cd73f65f38f1c1
Simon Cozens <simon@simon-cozens.org>
2016-02-26 17:01:26 +1100

    Transform an entire directory into JSON; also parse more interations of the site’s design.

--- node2json.rb
+++ node2json.rb
@@ -2,71 +2,111 @@ require 'reverse_markdown'
 require 'nokogiri'
 require 'json'
 require 'URI'
-doc = Nokogiri::HTML(STDIN)
-r = doc.root
-node = {}
 
-r.css("img").each do |i|
-  i["src"] = URI.join("http://web.archive.org/",i.attr("src")).to_s
-end
+def file2json(f)
+  id = f.match(/node\/(\d+).html/)
+  if not id then return end
+  id = id[1]
+  outhandle = File.open("json/#{id}.json", "w")
 
-r.css("div.content a").each do |l|
-  # Make links into other typophile nodes into links into our system
-  # otherwise...
-  l["href"] = URI.join("http://web.archive.org/",l.attr("href")).to_s
-end
+  doc = Nokogiri::HTML(File.open(f))
+  r = doc.root
+  node = {"id" => id}
 
-title = r.css("title")
-if title.first
-  title = title.first.content.gsub(/ \| Typophile/,"").gsub(/^\s*\|\s*/,"").strip
-  node["title"] = ReverseMarkdown.convert(title).strip
-end
-
-forum = r.css("div.breadcrumb a")
-node["forum"] = ReverseMarkdown.convert(forum.last.content).strip if forum.first
-
-tags = r.css("li[class^=taxonomy]")
-node["tags"] = tags.map{ |t| ReverseMarkdown.convert(t.content).strip } if tags
-
-content = r.css(".node div.content")
-node["content"] = ReverseMarkdown.convert(content.first) if content.first
-
-info = r.css(".node div.info")
-author = info.children[0].to_s.strip
-time = info.children[2].to_s.strip
-
-if author.length < 1 then
-  info = r.css("span.submitted")
-  author = info.children[1].children[0].to_s
-  time = info.children[-1].to_s.strip
-  uid = info.children[1]["href"].gsub(/.*user\//,"")
-else
-  uid = r.css("div.node .picture a").first["href"].gsub(/.*user\//,"")
-end
-
-node["author"] = author
-node["time"] = time
-node["uid"] = uid
-# XXX author ID
-
-node["comments"] = []
-r.css("div.comment").each do |c|
-  comment = {}
-  metadata = c.css("div.author")
-  if metadata.children[0] then
-    author = metadata.children[0]
-    time = metadata.children[3].to_s
-  else
-    metadata = c.css("div.info")
-    author = metadata.children[1]
-    time = metadata.children[5].children[0].to_s
+  r.css("img").each do |i|
+    i["src"] = URI.join("http://web.archive.org/",i.attr("src")).to_s
   end
-  comment["author_name"] = author.children[0].to_s
-  comment["author_uid"] =  author.attr("href").gsub(/.*user\//,"")
-  comment["time"] = time
-  content = c.css("div.content")
-  comment["content"] = ReverseMarkdown.convert(content.to_s)
-  node["comments"] << comment
+
+  r.css("div.content a").each do |l|
+    # Make links into other typophile nodes into links into our system
+    # otherwise...
+    begin
+      uri = URI.parse(l.attr("href"))
+    rescue
+      uri = URI.parse(URI.escape(l.attr("href") || ""))
+    end
+    l["href"] = URI.join("http://web.archive.org/",uri).to_s
+  end
+
+  title = r.css("title")
+  if title.first
+    title = title.first.content.gsub(/ \| Typophile/,"").gsub(/^\s*\|\s*/,"").strip
+    node["title"] = ReverseMarkdown.convert(title).strip
+    if title == ";) BRB" then
+      puts("Uhoh, #{id} was a bad copy")
+      return
+    end
+  end
+
+  forum = r.css("div.breadcrumb a")
+  node["forum"] = ReverseMarkdown.convert(forum.last.content).strip if forum.first
+
+  tags = r.css("li[class^=taxonomy]")
+  node["tags"] = tags.map{ |t| ReverseMarkdown.convert(t.content).strip } if tags
+
+  content = r.css(".node div.content")
+  node["content"] = ReverseMarkdown.convert(content.first) if content.first
+
+  info = r.css(".node div.info")
+  author = info.children[0].to_s.strip
+  time = info.children[2].to_s.strip
+  if author == "Submitted by" then
+    author = info.children[1].to_s.strip
+  end
+
+  if author.length < 1 then
+    info = r.css("span.submitted")
+    author = info.children[1]
+    if author then
+      author = author.children[0].to_s
+      time = info.children[-1].to_s.strip
+      if info.children[1]["href"] then # May be a guest
+        uid = info.children[1]["href"].gsub(/.*user\//,"")
+      end
+    end
+  else
+    picture = r.css("div.node .picture a")
+    if picture.first then
+      uid = picture.first["href"].gsub(/.*user\//,"")
+    elsif info.children.count > 1
+      uid = info.children[1]["href"].gsub(/.*user\//,"")
+    end
+  end
+
+  node["author"] = author
+  node["time"] = time
+  node["uid"] = uid
+  # XXX author ID
+
+  node["comments"] = []
+  r.css("div.comment").each do |c|
+    comment = {}
+    metadata = c.css("div.author")
+    if metadata.children[0] then
+      author = metadata.children[0]
+      time = metadata.children[3].to_s
+    else
+      metadata = c.css("div.info")
+      author = metadata.children[1]
+      time = metadata.children[5]
+      if time then
+        time = time.children[0].to_s
+      else
+        time=metadata.children[3].to_s
+      end
+    end
+    comment["time"] = time
+    content = c.css("div.content")
+    comment["content"] = ReverseMarkdown.convert(content.to_s)
+    node["comments"] << comment
+  end
+
+  outhandle.puts(JSON.pretty_generate(node))
+  outhandle.close()
 end
 
-puts JSON.pretty_generate(node)
+Dir.glob("typophile.com/node/*.html").each do |f|
+  puts(f)
+  file2json(f)
+end
+
